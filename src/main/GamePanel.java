f3b5373670c6fd794c79d05fc127057613a221e0
Grégory Clain <g.clain69@gmail.com>
2022-06-29 20:56:28 +0200

    binding mouse inputs to rectangle

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -11,9 +11,9 @@ import java.awt.event.KeyListener;
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int xDelta = 0, yDelta = 0;
+    private int xDelta = 100, yDelta = 100;
     public GamePanel() {
-        mouseInputs = new MouseInputs();
+        mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
@@ -28,9 +28,15 @@ public class GamePanel extends JPanel {
         this.yDelta += value;
         repaint();
     }
+
+    public void setRectPos(int x,int y){
+        this.xDelta = x;
+        this.yDelta = y;
+        repaint();
+    }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.fillRect(100 + xDelta,100 + yDelta,200,50);
+        g.fillRect( xDelta, yDelta,200,50);
 
     }
 }
